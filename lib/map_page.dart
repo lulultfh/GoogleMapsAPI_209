f275ecd7b66af05e8bfa1c7f8f81f4e1703c4c2b
lulultfh <punyalulu05@gmail.com>
2026-05-08 10:49:58 +0700

    feat: menambahkan tap handler untuk select lokasi

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -68,6 +68,30 @@ class _MapPageState extends State<MapPage> {
     return Geolocator.getCurrentPosition();
   }
 
+  Future<void> _onTap(LatLng latlng) async {
+    final placemarks = await placemarkFromCoordinates(
+      latlng.latitude,
+      latlng.longitude,
+    );
+    final p = placemarks.first;
+    setState(() {
+      _pickedMarker = Marker(
+        markerId: const MarkerId('picked'),
+        position: latlng,
+        infoWindow: InfoWindow(
+          title: p.name?.isNotEmpty == true ? p.name : 'Lokasi dipilih',
+          snippet: '${p.street}, ${p.locality}'
+        ),
+      );
+    });
+    final ctrl = await _ctrl.future;
+    await ctrl.animateCamera(CameraUpdate.newLatLngZoom(latlng, 16));
+
+    setState(() {
+      _pickedAddress = '${p.name}, ${p.street}, ${p.locality}, ${p.country}, ${p.postalCode}';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
