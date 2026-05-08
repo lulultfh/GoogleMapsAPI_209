2cbf95aeb979fcc3a2c70ee3482000ad8ab5734e
lulultfh <punyalulu05@gmail.com>
2026-05-08 10:26:23 +0700

    feat: menambahkan void initState

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,6 +12,19 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController>_ctrl = Completer();
+  Marker? _pickedMarker;
+  String? _pickedAddress;
+  String? _currentAddress;
+  CameraPosition? _initialCamera;
+  Position? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupLocation();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Container();
